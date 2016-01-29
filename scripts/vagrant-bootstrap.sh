ff5fd813a3822011e776af182c07217bfb231bbd
Oskar Risberg <kokarn@gmail.com>
2016-01-29 21:23:15 +0100

    Remember to install the needed dependencies for the backend when we start vagrant

--- scripts/vagrant-bootstrap.sh
+++ scripts/vagrant-bootstrap.sh
@@ -12,6 +12,9 @@ apt-get install -y npm
 # Install dependencies needed for node-canvas
 apt-get install -y libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev build-essential g++
 
+cd /vagrant
+npm install
+
 # Link "nodejs" to "node" on Ubuntu
 if [ ! -L /usr/bin/node ];
 then
