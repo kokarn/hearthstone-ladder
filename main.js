06ae635df2c370af9a4c85d5a1a890872717602d
Oskar Risberg <kokarn@gmail.com>
2016-02-02 10:57:54 +0100

    Run a number of images in parallel but help the garbage collector

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let twitch = require( './modules/Twitch.js' );
 let imageBaseUrl = 'http://static-cdn.jtvnw.net/previews-ttv/live_user_{channel}-{width}x{height}.jpg';
 let detectionsStarted = 0;
 let detectionsDone = 0;
-let detectionsRunningLimit = 1;
+let detectionsRunningLimit = 50;
 let twitchData;
 
 function getTwitchImageUrl( channel, width, height ){
@@ -15,29 +15,40 @@ function getTwitchImageUrl( channel, width, height ){
         .replace( '{height}', height );
 }
 
-function startDetectionByIndex( index ){
-    startDetection( twitchData.streams[ index ].channel.name );
-}
-
 function startDetection( channel ){
     var finder = new CharacterFinder( getTwitchImageUrl( channel, 1920, 1080 ), 145, 60 );
     detectionsStarted = detectionsStarted + 1;
 
     finder.onComplete( function( file, result ){
         console.log( file + ' matched as ' + result );
-        if( detectionsStarted < twitchData.streams.length ){
+        detectionsDone = detectionsDone + 1;
 
-            // Trying to make sure the call stack doesn't get filled,
-            // doesn't work tho...
-            process.nextTick( () => {
-                startDetectionByIndex( detectionsStarted - 1 )
-            });
-        }
+        finder = null;
     });
 }
 
+function startDetectionByIndex( index ){
+    if( detectionsStarted - detectionsDone > detectionsRunningLimit ){
+        process.nextTick( () => {
+            startDetectionByIndex( index );
+        });
+
+        return true;
+    }
+
+    if( detectionsStarted < twitchData.streams.length ){
+        process.nextTick(() => {
+            if( twitchData.streams[ index ] ){
+                startDetection( twitchData.streams[ index ].channel.name );
+            }
+        });
+    }
+}
+
+
+
 console.log( 'Loading Hearthstone streams' );
-twitch( 'streams?game=Hearthstone:%20Heroes%20of%20Warcraft', function( error, response ){
+twitch( 'streams?game=Hearthstone:%20Heroes%20of%20Warcraft&limit=100', function( error, response ){
     if( error ){
         throw error;
     }
