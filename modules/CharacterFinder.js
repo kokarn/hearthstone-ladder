36a0f433771d8cd8e806ace0f06bdffde3326cee
Oskar Risberg <kokarn@gmail.com>
2016-02-05 14:54:52 +0100

    Remove unneccesary console.log from detector

--- modules/CharacterFinder.js
+++ modules/CharacterFinder.js
@@ -34,7 +34,6 @@ var CharacterFinder = function( filePath, width, height ){
         });
 
         response.on( 'end', function() {
-            console.log( filePath );
             _this.setImg( data.read() );
 
             data = null;
