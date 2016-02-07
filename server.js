7fd90d586fb94309e014e422047ae90bdd91fa54
Oskar Risberg <kokarn@gmail.com>
2016-02-07 19:02:45 +0100

    Add cleanup endpoint
    This is just a test, won't actually perform any cleanup

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ var hearthstone = require( './modules/Hearthstone.js' );
 
 app.use( express.static( path.join( __dirname, '/www' ) ) );
 
+function pad( n, width, z ) {
+    z = z || '0';
+    n = String( n );
+    return n.length >= width ? n : new Array( width - n.length + 1 ).join( z ) + n;
+}
+
+function getMatchImagePath( channel, timestamp ){
+    let startMoment = moment( timestamp );
+    return 'tmp/' + startMoment.year() + '-' + pad( startMoment.month() + 1, 2 ) + '-' + pad( startMoment.date(), 2 ) + '-' + startMoment.hour() + ( startMoment.minute() - ( startMoment.minute() % 5 ) ) + '.jpg';
+}
+
 app.get( '/data/*', function( request, response ){
     var connection = mysql.createConnection({
         host : config.database.host,
@@ -144,6 +155,68 @@ app.get( '/check', function( request, response ){
     htmlResponse = htmlResponse + '</ul>';
 
     response.send( htmlResponse );
+});
+
+app.get( '/cleanup', function( request, response ){
+    var connection = mysql.createConnection({
+        host : config.database.host,
+        user : config.database.user,
+        port: config.database.port,
+        password : config.database.password,
+        database : config.database.name
+    });
+
+    var htmlResponse = '';
+
+    connection.connect();
+
+    connection.query( `
+        SELECT
+            channel,
+            timestamp
+        FROM
+            matches
+        ORDER BY
+            channel,
+            timestamp`,
+        function( error, rows, fields ){
+            if( error ) {
+                throw error;
+            }
+
+            let currentChannel = false;
+            let lastTimestamp = false;
+            let timediffValidity = 3600000;
+            for( let i = 0; i < rows.length - 1; i = i + 1 ){
+
+                if( rows[ i + 1 ].channel === rows[ i ].channel ){
+                    // If the next channel is the same as this check the diff timestamp
+                    if( moment( rows[ i + 1 ].timestamp ).diff( moment( rows[ i ].timestamp ) ) > timediffValidity ){
+                        // The timestamp is more than timediffValidity offset from the previous timestamp so probably a one-off match
+                        console.log( 'Clear up', rows[ i ].timestamp, ' its more than 1 hour from ', rows[ i + 1 ].timestamp );
+                        htmlResponse = htmlResponse + '<img src="' + getMatchImagePath( rows[ i ].channel, rows[ i ].timestamp ) + '">';
+                    }
+                } else if( rows[ i - 1 ].channel === rows[ i ].channel ){
+                    // If the next channel isn't the same, check if the previous one is
+                    if( moment( rows[ i ].timestamp ).diff( moment( rows[ i - 1 ].timestamp ) ) > timediffValidity ){
+                        // Check if that timestamp is more than one newer than the latest one.
+                        console.log( 'Clear up', rows[ i ].timestamp, ' its more than 1 hour from', rows[ i - 1 ].timestamp );
+                        htmlResponse = htmlResponse + '<img src="' + getMatchImagePath( rows[ i ].channel, rows[ i ].timestamp ) + '">';
+                    }
+                } else {
+                    // Neither the next nor the previous channel match is the same
+                    if( moment( rows[ i ].timestamp ).diff( moment() ) > timediffValidity ){
+                        // If the next channel isn't the same as this, this is the last timestamp for that channel.
+                        // Check if that timestamp is more than one newer than the next last one.
+                        console.log( 'Clear up', rows[ i ].timestamp, ' its more than 1 hour old' );
+                        htmlResponse = htmlResponse + '<img src="' + getMatchImagePath( rows[ i ].channel, rows[ i ].timestamp ) + '">';
+                    }
+                }
+            }
+
+            response.send( htmlResponse );
+        }
+    );
 });
 
 app.get( '/check/*', function( request, response ){
